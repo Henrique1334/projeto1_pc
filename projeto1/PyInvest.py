161212bb5d4b6d96ee61b72473865f0db2a022be
Henrique1334 <riquerodrigues49@gmail.com>
2026-03-13 19:37:06 -0300

    Update PyInvest.py

--- projeto1/PyInvest.py
+++ projeto1/PyInvest.py
@@ -34,7 +34,83 @@ montante_lci = (capital * math.pow ( 1 + taxa_lci), meses) + (aporte+ meses)
 #poupanca
 taxa_poupanca= 0.005
 montante_poupanCa = (capital * math.pow(1 + taxa_poupanca), meses) + (aporte * meses)
+montante = (capital * math.pow((1 + taxa_fii), meses) + (aporte * meses))
+variacao = random.uniform(-0.03, 0.03) 
+valor_final = montante * (1 + variacao)
 
+
+variacao_1= random.uniform(-0.03,0.03)
+variacao_2= random.uniform(-0.03,0.03)
+variacao_3= random.uniform(-0.03,0.03)
+variacao_4= random.uniform(-0.03,0.03)
+variacao_5= random.uniform(-0.03,0.03)
+
+FII1= (capital * math.pow(1 + taxa_fii, meses) + (aporte * meses)) *(1 + variacao_1)
+FII2= (capital * math.pow(1 + taxa_fii, meses) + (aporte * meses)) *(1 + variacao_2)
+FII3= (capital * math.pow(1 + taxa_fii, meses) + (aporte * meses)) *(1 + variacao_3)
+FII4= (capital * math.pow(1 + taxa_fii, meses) + (aporte * meses)) *(1 + variacao_4)
+FII5= (capital * math.pow(1 + taxa_fii, meses) + (aporte * meses)) *(1 + variacao_5)
+
+taxa_fii= (FII1,FII2,FII3,FII4,FII5)
+
+media_FII = statistics.mean(taxa_fii)
+mediana_FII = statistics.median(taxa_fii)
+desvio_FII = statistics.stdev(taxa_fii)
+
+#datas
+
+data_simulacao= datetime.datetime.now()
+dias= meses * 30
+data_resgate= data_simulacao + datetime.timedelta(days= dias)
+
+#meta 
+meta_atingida= media_FII>= meta
+
+#formatação moeda
+total_format= locale.currency(total_investido, grouping= True)
+cdb_format= locale.currency(montante_cdb_liquido, grouping= True)
+lci_format= locale.currency(montante_lci, grouping= True)
+poup_format= locale.currency(montante_poupanca, grouping= True)
+
+media_format= locale.currency(media_FII, grouping= True)
+mediana_format= locale.currency(mediana_FII, grouping= True)
+desvio_format= locale.currency(desvio_FII, grouping= True)
+
+#grafico ASCII
+graf_cdb= "█" * int(montante_cdb_liquido / 1000)
+graf_lci= "█" * int(montante_lci / 1000)
+graf_poup= "█" * int(montante_poupanca / 1000)
+graf_FII= "█" * int(media_FII / 1000)
+
+#relatório
+
+print("relatório de investimento")
+
+print("data de simulação: " ,data_simulacao.strftime("%d/%m/%y"))
+print("data estimada de resgate: " ,data_resgate.strftime("%d/%m/%y"))
+
+print("total investido: ", total_format)
+
+print("resultados")
+
+print("cdb:", cdb_format)
+print("lci:", lci_format)
+print("poupança", poup_format)
+print("FII (media): ", media_format)
+
+print ("estatisticas FII")
+
+print("media: ", media_format)
+print("mediana:", mediana_format)
+print("desvio padrão: ", desvio_format)
+
+print("meta financeira atingida: ", meta_atingida)
+
+print("grafico comparativo")
+print("cdb", graf_cdb)
+print("lci", graf_lci)
+print("poupança: ", graf_poup)
+print("FII", graf_FII)
 #FII - SIMUlACOES
 
 
